c63913367ddb77d38e5a72177dd630adaf5ce324
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-20 17:12:08 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AlzaSyCdsrbbxoPvD5XdjDog006HPErf-DWKZAO",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "my-shorts-50232",
+  appId: "1:300830667303 android:9b9ba2ce17104d0c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
