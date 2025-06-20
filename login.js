1f43a9f3549348f249f98c7ed3dbad87f70f476a
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-20 17:26:51 +0530

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AlzaSyCdsrbbxoPvD5XdjDog006HPErf-DWKZAO",
-  authDomain: "https://jaypal1184.github.io/my-shorts/.firebaseapp.com",
-  projectId: "my-shorts-50232",
-  appId: "1:300830667303 android:9b9ba2ce17104d0c"
-};
-
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
