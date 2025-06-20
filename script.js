512986dec32f5bbdf184c4834403b773ce9285ac
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-20 18:14:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Future buttons functionality
 console.log("Script loaded!");
+function playVideo(src) {
+  const popup = document.getElementById("videoPopup");
+  const video = document.getElementById("popupVideo");
+  video.src = src;
+  popup.style.display = "flex";
+  video.play();
+}
+
+function closePopup() {
+  const popup = document.getElementById("videoPopup");
+  const video = document.getElementById("popupVideo");
+  video.pause();
+  video.src = "";
+  popup.style.display = "none";
+}
