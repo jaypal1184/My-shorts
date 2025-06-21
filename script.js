237efc82a559543880e38c060f33d576b3f2c7ec
jaypal1184 <jaypalpatel1184@gmail.com>
2025-06-21 10:32:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Future buttons functionality
 console.log("Script loaded!");
+function updateUI() {
+    if(currentUser) {
+        const profilePic = document.getElementById('profilePic');
+        if (profilePic && currentUser.photoURL) {
+            profilePic.src = currentUser.photoURL;
+        }
+    }
+}
+
